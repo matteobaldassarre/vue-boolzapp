a28dd398118714ebac1f66008a7dd41af4c85224
matteobaldassarre <matteobaldassarre29@gmail.com>
2021-04-23 21:11:58 +0200

    added dropdown

--- js/script.js
+++ js/script.js
@@ -16,16 +16,19 @@ var app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
+                        userClick: false,
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
+                        userClick: false,
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
+                        userClick: false,
                         status: 'received'
                     }
                 ],
@@ -39,16 +42,19 @@ var app = new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
+                        userClick: false,
                         status: 'sent'
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
+                        userClick: false,
                         status: 'received'
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        userClick: false,
                         status: 'sent'
                     }
                 ],
@@ -62,16 +68,19 @@ var app = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
+                        userClick: false,
                         status: 'received'
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
+                        userClick: false,
                         status: 'sent'
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
+                        userClick: false,
                         status: 'received'
                     }
                 ],
@@ -85,11 +94,13 @@ var app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        userClick: false,
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
+                        userClick: false,
                         status: 'received'
                     }
                 ],
@@ -114,7 +125,8 @@ var app = new Vue({
                 this.contacts[this.contactIndex].messages.push({
                     date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
                     text: this.writeMessageInput,
-                    status: 'sent'
+                    status: 'sent',
+                    userClick: false
                 });
             }
             this.writeMessageInput = ''
@@ -122,10 +134,19 @@ var app = new Vue({
             setTimeout( () => { 
                 this.contacts[this.contactIndex].messages.push({
                     date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-                    text: 'ok',
-                    status: 'received'
+                    text: 'ok, volentieri!',
+                    status: 'received',
+                    userClick: false
                 });
             }, 1000);
+        },
+        toggleDropdown(index) {
+            let indice = index;
+            this.contacts[this.contactIndex].messages.forEach((element, index) => {
+                if (indice == index) {
+                    element.userClick = !element.userClick;
+                } 
+            });
         }
     }
 });
