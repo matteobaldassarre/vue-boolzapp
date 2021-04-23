701d1c6265304d1c679feea0efd15d233fcbd430
matteobaldassarre <matteobaldassarre29@gmail.com>
2021-04-23 22:56:51 +0200

    you can now delete sent and received messages

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ var app = new Vue({
     data: {
         contactIndex: 0,
         searchContact: '',
-        writeMessageInput: '', 
+        writeMessageInput: '',
+        active: false,
         // Contatti Whatsapp
         contacts: [
             // Michele
@@ -17,18 +18,21 @@ var app = new Vue({
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
                         userClick: false,
+                        shown: true,
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
                         userClick: false,
+                        shown: true,
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
                         userClick: false,
+                        shown: true,
                         status: 'received'
                     }
                 ],
@@ -43,18 +47,21 @@ var app = new Vue({
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
                         userClick: false,
+                        shown: true,
                         status: 'sent'
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
                         userClick: false,
+                        shown: true,
                         status: 'received'
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         userClick: false,
+                        shown: true,
                         status: 'sent'
                     }
                 ],
@@ -69,18 +76,21 @@ var app = new Vue({
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
                         userClick: false,
+                        shown: true,
                         status: 'received'
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
                         userClick: false,
+                        shown: true,
                         status: 'sent'
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
                         userClick: false,
+                        shown: true,
                         status: 'received'
                     }
                 ],
@@ -95,12 +105,14 @@ var app = new Vue({
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
                         userClick: false,
+                        shown: true,
                         status: 'sent'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
                         userClick: false,
+                        shown: true,
                         status: 'received'
                     }
                 ],
@@ -126,7 +138,8 @@ var app = new Vue({
                     date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
                     text: this.writeMessageInput,
                     status: 'sent',
-                    userClick: false
+                    userClick: false,
+                    shown: true
                 });
             }
             this.writeMessageInput = ''
@@ -136,7 +149,8 @@ var app = new Vue({
                     date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
                     text: 'ok, volentieri!',
                     status: 'received',
-                    userClick: false
+                    userClick: false,
+                    shown: true
                 });
             }, 1000);
         },
@@ -145,7 +159,21 @@ var app = new Vue({
             this.contacts[this.contactIndex].messages.forEach((element, index) => {
                 if (indice == index) {
                     element.userClick = !element.userClick;
-                } 
+                }
+            });
+        },
+        closeDropdown () {
+            this.contacts[this.contactIndex].messages.forEach((element) => {
+                element.userClick = false;
+            });
+        },
+        deleteMessage(index) {
+            let indice = index;
+            this.contacts[this.contactIndex].messages.forEach((element, index) => {
+                if (indice == index) {
+                    element.shown = false;
+                    element.userClick = false;
+                }
             });
         }
     }
